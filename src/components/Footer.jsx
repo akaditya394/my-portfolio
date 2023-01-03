d79d23b8a4f57866f5b6adee0d813e72da20386b
Aditya <akaditya394@gmail.com>
2023-01-03 16:52:04 +0530

    footer made responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-[#f2f2f2] absolute w-screen bottom-0 hidden md:block text-[#70757a]">
+    <div
+      className="bg-[#f2f2f2] absolute w-screen \
+    bottom-0 text-[#70757a]"
+    >
       <div className="px-3 py-3 border-b-2">
         <p className="text-sm text-[#70757a]">India</p>
       </div>
-      <div className="flex flex-row items-center justify-between px-3 py-3">
+      <div className="flex flex-col md:flex-row items-center justify-between px-3 py-3">
         <div className="flex flex-row gap-2">
           <p className="text-sm text-[#70757a]">About</p>
           <p className="text-sm text-[#70757a]">Advertising</p>
